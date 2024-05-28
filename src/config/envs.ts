3ea5aba7f3e19968370edc163bbf5dfc8671df1d
Daniel Vidal <daniel.vidal@globant.com>
2024-05-28 07:22:32 -0500

    Integration between orders and payments has been created

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,15 +2,17 @@ import 'dotenv/config';
 import * as joi from 'joi';
 
 interface EnvVars {
+  NATS_SERVERS: string[];
   PORT: number;
   STRIPE_API_KEY: string;
+  STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
   STRIPE_SUCCESS_URL: string;
-  STRIPE_CANCEL_URL: string;
 }
 
 const envsSchema = joi
   .object({
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
     PORT: joi.number().required(),
     STRIPE_API_KEY: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
@@ -21,6 +23,7 @@ const envsSchema = joi
 
 const { error, value } = envsSchema.validate({
   ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
@@ -30,6 +33,7 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
+  natsServers: envVars.NATS_SERVERS,
   port: envVars.PORT,
   stripeApiKey: envVars.STRIPE_API_KEY,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
